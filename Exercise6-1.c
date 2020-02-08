3aa0edef1bcd0556fbf1457fd91cc8926aea5827
Vanin Ferrall <49698503+Ferrallv@users.noreply.github.com>
2020-02-07 20:54:18 -0500

    Create Exercise6-1.c

---
+++ Exercise6-1.c
@@ -0,0 +1,10 @@
+// A year on Jupiter ( the time it takes Jupiter to make one full rotation around the sun) takes about 12 Earth years. Write a program
+// that converts Jovian years to earth years. Have the user specify the number of Jovian years. Allow fractional years.
+
+#include <std.io.h>
+
+int main (void)
+{
+  double jovian, earth;
+  
+}
