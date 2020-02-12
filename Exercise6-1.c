94a089e177dbbea03037b01281001640d7d2db9c
Vanin Ferrall <vanin.ferrall@gmail.com>
2020-02-11 21:01:11 -0500

    wrote a program

--- Exercise6-1.c
+++ Exercise6-1.c
@@ -5,6 +5,18 @@
 
 int main (void)
 {
-  double jovian, earth;
-  
+  	double jovian, earth;
+  	char leavingNow;
+
+  	leavingNow = ''
+
+  	while leavingNow != 'exit';
+		printf("Please enter a number greater than 0, this will represent Jovian years\n");
+		printf("Then I can tell you how many Earth years that will be \n");
+		scanf(%lf, &jovian);
+		  
+		earth = jovian*12;
+		printf("You're looking at %s Earth years. Wow!\n", earth);
+		printf("If you would like to stop, type 'exit'");
+		scanf(%c, &leavingNow);
 }
