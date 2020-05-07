273751ae91945238ab1470bbd2b1f1ee53746c38
Ferrallv <vanin.ferrall@gmail.com>
2020-05-07 14:39:21 -0400

    some lessons

---
+++ GO_lessons_notes/jedi_exc/package-trial/dog.go
@@ -0,0 +1,9 @@
+// Package dog is used to easily convert human years to dog years.
+package dog
+
+/* Years accepts human years as an int and 
+returns an int of the dog years
+*/
+func Years(i int) int {
+	return i*7
+}
---
+++ GO_lessons_notes/jedi_exc/package-trial/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"dog"
+)
+
+func main() {
+	fmt.Println("I am 30. I am ", dog.Years(30), "in dog years.")
+}
