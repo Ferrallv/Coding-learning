c6ad1106ac1085319d79b30f161ef25b9ba737f8
Ferrallv <vanin.ferrall@gmail.com>
2020-05-13 19:03:19 -0400

    learning more webdev

---
+++ GO/go_webdev/net_http/request/retrieving_form_vals/practice_main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	delete(req.Form, "submit-btn")
+	tpl.ExecuteTemplate(w, "practice_index.gohtml", req.Form)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("practice_index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
