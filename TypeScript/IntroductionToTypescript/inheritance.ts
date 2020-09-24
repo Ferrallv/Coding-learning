cb65b798f7087dac141089f1a1c6d82cc53830ae
Vanin Ferrall <vanin.ferrall@gmail.com>
2020-09-24 18:56:15 -0400

    some code lernin'

---
+++ TypeScript/IntroductionToTypescript/inheritance.ts
@@ -0,0 +1,21 @@
+class Person {
+	name: string;
+	constructor(name: string) {
+		this.name = name;
+	}
+	dance(){
+		console.log(this.name +  " is dancing");
+	}
+}
+
+var bran = new Person("Bran");
+bran.dance()
+
+class AwesomePerson extends Person {
+	dance() {
+		console.log("SO AWESOME");
+	}
+}
+
+var robb = new AwesomePerson("Robb")
+robb.dance()
